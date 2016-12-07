01fe5a88f19abe7400af323a00837565a46d2df1
Philip Arndt <git@p.arndt.io>
2016-12-07 22:25:02 +1300

    Remove stylesheet and javascript registers

--- core/lib/refinery/core/configuration.rb
+++ core/lib/refinery/core/configuration.rb
@@ -2,44 +2,46 @@ module Refinery
   module Core
     include ActiveSupport::Configurable
 
-    config_accessor :rescue_not_found, :s3_backend, :base_cache_key, :site_name,
-                    :google_analytics_page_code, :authenticity_token_on_frontend,
-                    :dragonfly_secret, :javascripts, :stylesheets, :mounted_path,
-                    :s3_bucket_name, :s3_region, :s3_access_key_id,
-                    :s3_secret_access_key, :force_ssl, :backend_route,
-                    :dragonfly_custom_backend_class, :dragonfly_custom_backend_opts,
-                    :visual_editor_javascripts, :visual_editor_stylesheets,
-                    :plugin_priority, :refinery_logout_path
+    config_accessor :authenticity_token_on_frontend,
+                    :backend_route,
+                    :base_cache_key,
+                    :dragonfly_custom_backend_class,
+                    :dragonfly_custom_backend_opts,
+                    :dragonfly_secret,
+                    :force_ssl,
+                    :google_analytics_page_code,
+                    :mounted_path,
+                    :plugin_priority,
+                    :refinery_logout_path,
+                    :rescue_not_found,
+                    :s3_access_key_id,
+                    :s3_backend,
+                    :s3_bucket_name,
+                    :s3_region,
+                    :s3_secret_access_key,
+                    :site_name,
+                    :visual_editor_javascripts,
+                    :visual_editor_stylesheets
 
-    self.rescue_not_found = false
-    self.s3_backend = false
-    self.base_cache_key = :refinery
-    self.site_name = "Company Name"
-    self.google_analytics_page_code = "UA-xxxxxx-x"
     self.authenticity_token_on_frontend = false
-    self.dragonfly_secret = Array.new(24) { rand(256) }.pack('C*').unpack('H*').first
-    self.javascripts = []
-    self.stylesheets = []
-    self.s3_bucket_name = ENV['S3_BUCKET']
-    self.s3_region = ENV['S3_REGION']
-    self.s3_access_key_id = ENV['S3_KEY']
-    self.s3_secret_access_key = ENV['S3_SECRET']
-    self.force_ssl = false
     self.backend_route = "refinery"
-    self.mounted_path = "/"
+    self.base_cache_key = :refinery
     self.dragonfly_custom_backend_class = ''
     self.dragonfly_custom_backend_opts = {}
+    self.dragonfly_secret = Array.new(24) { rand(256) }.pack('C*').unpack('H*').first
+    self.force_ssl = false
+    self.google_analytics_page_code = "UA-xxxxxx-x"
+    self.mounted_path = "/"
+    self.plugin_priority = []
+    self.rescue_not_found = false
+    self.s3_access_key_id = ENV['S3_KEY']
+    self.s3_backend = false
+    self.s3_bucket_name = ENV['S3_BUCKET']
+    self.s3_region = ENV['S3_REGION']
+    self.s3_secret_access_key = ENV['S3_SECRET']
+    self.site_name = "Company Name"
     self.visual_editor_javascripts = []
     self.visual_editor_stylesheets = []
-    self.plugin_priority = []
-
-    def config.register_javascript(name)
-      self.javascripts << name
-    end
-
-    def config.register_stylesheet(*args)
-      self.stylesheets << Stylesheet.new(*args)
-    end
 
     def config.register_visual_editor_javascript(name)
       self.visual_editor_javascripts << name
@@ -60,14 +62,6 @@ module Refinery
         [mounted_path.gsub(%r{/\z}, ''), backend_route].join("/")
       end
 
-      def clear_javascripts!
-        self.javascripts = []
-      end
-
-      def clear_stylesheets!
-        self.stylesheets = []
-      end
-
       def dragonfly_custom_backend?
         config.dragonfly_custom_backend_class.present?
       end
@@ -82,15 +76,6 @@ module Refinery
 
       def wymeditor_whitelist_tags=(tags)
         raise "Please ensure refinerycms-wymeditor is being used and use Refinery::Wymeditor.whitelist_tags instead of Refinery::Core.wymeditor_whitelist_tags"
-      end
-    end
-
-    # wrapper for stylesheet registration
-    class Stylesheet
-      attr_reader :options, :path
-      def initialize(*args)
-        @options = args.extract_options!
-        @path = args.first if args.first
       end
     end
 
